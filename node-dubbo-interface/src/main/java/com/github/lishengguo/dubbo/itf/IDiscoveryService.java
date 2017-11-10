0159d503e9618efaface1c898e25d81ec0237ba5
shenxy <outerman@163.com>
2017-11-10 17:22:53 +0800

    修改pom，准备推maven

--- node-dubbo-interface/src/main/java/com/github/lishengguo/dubbo/itf/IDiscoveryService.java
+++ node-dubbo-interface/src/main/java/com/github/lishengguo/dubbo/itf/IDiscoveryService.java
@@ -8,9 +8,6 @@ public interface IDiscoveryService {
 
 	/**
 	 * 接口类型序列化
-	 * 
-	 * @param className
-	 * @return
 	 */
 	public ArrayList<ClassDto> interfaceSerializer(String[] interfaceNames) throws Exception;
 }
